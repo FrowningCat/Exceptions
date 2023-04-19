cb3eabf8f1079bb5867459b1bfc0886f2f6b4444
FrowningCat <vsavinkov60@gmail.com>
2023-04-19 14:11:26 +0300

    add_homework3

---
+++ src/lesson_3/DataException.java
@@ -0,0 +1,13 @@
+package lesson_3;
+
+public class DataException extends Exception{
+    public DataException() {
+    }
+
+    public void dataException(String i) {
+        System.out.println("Ошибка: Неверный формат данных формата");
+        System.out.printf("Неверный формат: %s", i);
+        System.out.println();
+    }
+}
+
